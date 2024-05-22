225a5cb5bae00a07a075cdcb1014fcc8d790a493
sapel <santiago.apel@eynes.com.ar>
2024-05-21 22:29:16 -0300

    [IMP] prod implementation

--- controllers/webhook.py
+++ controllers/webhook.py
@@ -7,9 +7,12 @@ _logger = logging.getLogger(__name__)
 
 
 class WhatsappWebhookController(http.Controller):
-    @http.route('/whatsapp/webhook', type='json', auth='public', methods=['POST'], csrf=False)
+    #
+    @http.route('/api/v1/whatsapp/webhook', type='json', auth='none', methods=['POST'], csrf=False, cors="*")
     def whatsapp_webhook(self):
         payload = request.jsonrequest
+        #setear db en request.session.db y ponerle el name y dsps ya puedo user el request.env para acceder y modifcar todas las tablas (Pdb) request.env['crm.lead'].sudo().create({'name': 'charlyyyyy'})
         _logger.info("Received WhatsApp webhook: %s", json.dumps(payload))
         # Process the incoming data here
         return True
+
