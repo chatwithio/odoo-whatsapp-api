a8af81d5979c5c6ddba5492d38936a8c024d65ff
ilmunie <69804855+ilmunie@users.noreply.github.com>
2024-05-28 11:27:41 -0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -2,12 +2,3 @@
 from . import models
 from . import wizards
 from . import controllers
-
-def pre_init_check(cr):
-    from odoo.service import common
-    from odoo.exceptions import UserError
-    version_info = common.exp_version()
-    server_serie = version_info.get('server_serie')
-    # -*-  if server_serie != '15.0':
-    # -*-     raise UserError(('Module support Odoo Version 15.0 only and found ' + server_serie))
-    return True
